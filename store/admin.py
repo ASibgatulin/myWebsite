c6691bb7ba3eaa5338a6dd7eefeb87c2c481dfca
ASibgatulin <sibgatulin_a@mail.ru>
2026-06-07 21:11:43 +0300

    step8 залил статьи, товары, сделал карту сайта, изменил футер с менюшкой

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Category, Product, Store, Promotion
+from .models import ArticleCategory, Article
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Store)
 admin.site.register(Promotion)
+admin.site.register(ArticleCategory)
+admin.site.register(Article)
